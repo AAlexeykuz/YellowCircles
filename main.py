679fbd10baa79f7a2acf4d3db28312ebcaa8cc6d
Alexeykuz <alexeykuzmichenko@gmail.com>
2024-12-15 20:16:49 +0300

    random color

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import sys
 import random
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPainter, QBrush
+from PyQt6.QtGui import QPainter, QBrush, QColor
 from PyQt6.QtWidgets import QWidget, QApplication
-from PyQt6 import uic
+from ui import Ui_Form
 
 
-class DrawCircles(QWidget):
+class DrawCircles(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
         self.circles = []
+
     def paint(self):
         circle_radius = random.randint(20, 50)
         x = random.randint(0, self.width() - circle_radius * 2)
         y = random.randint(0, self.height() - circle_radius * 2)
-        self.circles.append((x, y, circle_radius))
+        random_color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, circle_radius, random_color))
         self.update()
 
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.begin(self)
-        qp.setBrush(QBrush(Qt.GlobalColor.yellow))
-        for x, y, radius in self.circles:
+        for x, y, radius, color in self.circles:
+            qp.setBrush(QBrush(color))
             qp.drawEllipse(x, y, radius * 2, radius * 2)
         qp.end()
 
